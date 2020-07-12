ecca4252721f727b9d43eaed33b187aaf76406e1
Max <gmax115230@gmail.com>
2020-07-12 10:57:24 +0300

    created Project

---
+++ Instagram Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Client
+//
+//  Created by Max on 12/07/2020.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
