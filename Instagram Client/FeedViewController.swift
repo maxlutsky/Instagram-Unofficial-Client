a4ab4b17f85ce741b61ec73334ee9bfa0a24936a
Max <gmax115230@gmail.com>
2020-08-03 12:05:29 +0300

    addedSimpleTableView

--- Instagram Client/FeedViewController.swift
+++ Instagram Client/FeedViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    let tableViewFeed: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = true
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .gray
+        createFeed()
+    }
+
+    func createFeed(){
+        view.addSubview(tableViewFeed)
+        tableViewFeed.translatesAutoresizingMaskIntoConstraints = false
+        tableViewFeed.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        tableViewFeed.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        tableViewFeed.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        tableViewFeed.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
     }
 }
 
